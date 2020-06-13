b13640c3563b56355adf18328ecca1497063f451
xuyuefeng@192.168.0.119:29418 <2670476383@qq.com>
2020-06-13 10:28:54 +0800

    yep

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public GameObject player;
+    public Vector2 Margin;
+    public Vector2 smoothing;
+
+    public bool IsFollowing { get; set; }
+    // Start is called before the first frame update
+    void Start()
+    {
+        IsFollowing = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var x = transform.position.x;
+        var y = transform.position.y;
+        if (IsFollowing)
+        {
+            if (Mathf.Abs(x - player.transform.position.x) > Margin.x)
+            {
+                x = Mathf.Lerp(x, player.transform.position.x, smoothing.x * Time.deltaTime);
+            }
+            if (Mathf.Abs(y - player.transform.position.y) > Margin.y)
+            {
+                y = Mathf.Lerp(y, player.transform.position.y, smoothing.y * Time.deltaTime);
+            }
+        }
+        transform.position = new Vector3(x, y, transform.position.z);
+    }
+}
---
+++ Assets/Scripts/OpossumController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpossumController : MonoBehaviour
+{
+    public float runSpeed = 40f;
+
+    private Rigidbody2D rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void OnTriggerEnter(Collider collider)
+    {
+        Debug.Log("OnTriggerEnter");
+    }
+    void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("OnCollisionEnter");
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Vector2 targetVelocity = new Vector2(-runSpeed * Time.fixedDeltaTime, rb.velocity.y);
+        rb.velocity = targetVelocity;
+    }
+}
